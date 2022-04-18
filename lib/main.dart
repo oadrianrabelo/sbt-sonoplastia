a91aa83856ff25cb2439d6329c16ed8a16e7325c
adrian.rabelo <adrian.mendes@tjro.jus.br>
2022-04-18 12:32:47 -0400

    reproduzindo som a partir do assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,57 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    setAudio();
+
+    audioPlayer.onDurationChanged.listen((newDuration) {
+      setState(() {
+        duration = newDuration;
+      });
+    });
+
+    audioPlayer.onAudioPositionChanged.listen((newPosition) {
+      setState(() {
+        position = newPosition;
+      });
+    });
+
+  }
+
+  Future setAudio() async {
+    // repeat when completed
+
+    final player = AudioCache(prefix: 'assets/audio/');
+    final url = await player.load('moo.mp3');
+    // load from asset
+    audioPlayer.setUrl(url.path, isLocal: true);
+  }
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,9 +64,48 @@ class MyApp extends StatelessWidget {
           child: ListView(
             children: [
               Card(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Tocar'),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 35,
+                      child: IconButton(
+                        icon: Icon(
+                          isPlaying ? Icons.pause : Icons.play_arrow,
+                        ),
+                        iconSize: 50,
+                          onPressed: () async {
+                            if (isPlaying) {
+                              await audioPlayer.pause();
+                            } else {
+                              await audioPlayer.resume();
+                            }
+                          },
+                      ),
+
+                    ),
+                    Slider(
+                      min: 0,
+                      max: duration.inSeconds.toDouble(),
+                      value: position.inSeconds.toDouble(),
+                      onChanged: (value) async {
+                        final position = Duration(seconds: value.toInt());
+
+                        await audioPlayer.seek(position);
+
+                        await audioPlayer.resume();
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(formatTime(position)),
+                          Text(formatTime(duration - position)),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -35,4 +118,16 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+String formatTime(Duration duration) {
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  final hours = twoDigits(duration.inHours);
+  final minutes = twoDigits(duration.inMinutes.remainder(60));
+  final seconds = twoDigits(duration.inSeconds.remainder(60));
+  return [
+    if (duration.inHours > 0) hours,
+    minutes,
+    seconds,
+  ].join(':');
 }
